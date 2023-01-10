1b23e25a212f35950952d88ad2d0b940c2e8844b
Your NameArniss5 <kat.dobr@gmail.com>
2023-01-10 11:29:28 +0000

    implement basic gameplay

--- src/App.js
+++ src/App.js
@@ -2,31 +2,54 @@ import React, {useState, useEffect, useRef} from 'react'
 import './App.css';
 
 function App() {
-
-  const [time, setTime] = useState(5)
-  const [wordcount, setWordCount] = useState(0)
+  const GAME_TIME = 5
+  const [time, setTime] = useState(GAME_TIME)
+  const [wordcount, setWordcount] = useState(0)
   const [isGameOn, setIsGameOn] = useState(false)
+  const [userInput, setUserInput] = useState('')
  
   useEffect(()=> {
     if(isGameOn && time > 0) {
       setTimeout(()=> {
         setTime(prevTime => prevTime - 1)
       }, 1000)
+    } else if (time === 0) {
+      endGame()
     }
     
   }, [time, isGameOn])
 
+  function handleChange(e) {
+    setUserInput(e.target.value)
+  }
+
+  function getWordCount() {
+    const wordsArr = userInput.split(" ").filter(word => word !== "")
+    return wordsArr.length
+  }
+
   function startGame() {
     setIsGameOn(true)
+    setTime(GAME_TIME)
+    setUserInput("")
+  }
+
+  function endGame() {
+    setWordcount(getWordCount())
+    setIsGameOn(false)
   }
 
   return (
     <div className="App">
       <h1>Speed Typing Game</h1>
       <p className='game-text'>How fast can you type?</p>
-      <textarea />
+      <textarea 
+        name="userInput"
+        value={userInput}
+        onChange={handleChange}
+      />
       <p className='game-text'>Time remaining: {time}</p>
-      <button onClick={startGame}>Start</button>
+      <button onClick={startGame} disabled={isGameOn}>Start</button>
       <p className='game-text'>Word count: {wordcount}</p>
     </div>
   );
