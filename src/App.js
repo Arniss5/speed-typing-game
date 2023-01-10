cfddc52160f7e85177ceeb3967f18a06ed543e23
Your NameArniss5 <kat.dobr@gmail.com>
2023-01-10 10:56:38 +0000

    add layout and styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Speed Typing Game</h1>
+      <p className='game-text'>How fast can you type?</p>
+      <textarea />
+      <p className='game-text'>Time remaining: ???</p>
+      <button>Start</button>
+      <p className='game-text'>Word count: ???</p>
     </div>
   );
 }
