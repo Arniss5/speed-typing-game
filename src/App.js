3c39f5d1a2893d8d3f3b63f5c93ef19aa8de2995
Your NameArniss5 <kat.dobr@gmail.com>
2023-01-10 11:12:17 +0000

    Add countdown

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import React, {useState, useEffect, useRef} from 'react'
 import './App.css';
 
 function App() {
+
+  const [time, setTime] = useState(5)
+  const [wordcount, setWordCount] = useState(0)
+  const [isGameOn, setIsGameOn] = useState(false)
+ 
+  useEffect(()=> {
+    if(isGameOn && time > 0) {
+      setTimeout(()=> {
+        setTime(prevTime => prevTime - 1)
+      }, 1000)
+    }
+    
+  }, [time, isGameOn])
+
+  function startGame() {
+    setIsGameOn(true)
+  }
+
   return (
     <div className="App">
       <h1>Speed Typing Game</h1>
       <p className='game-text'>How fast can you type?</p>
       <textarea />
-      <p className='game-text'>Time remaining: ???</p>
-      <button>Start</button>
-      <p className='game-text'>Word count: ???</p>
+      <p className='game-text'>Time remaining: {time}</p>
+      <button onClick={startGame}>Start</button>
+      <p className='game-text'>Word count: {wordcount}</p>
     </div>
   );
 }
